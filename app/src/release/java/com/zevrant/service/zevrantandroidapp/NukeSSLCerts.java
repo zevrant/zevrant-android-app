bf8a35bf7fee94e4c5876edc9c1bbbb9c8e8c9c2
zevrant <workmailtome@gmail.com>
2022-03-29 15:35:13 -0400

    added new variant specific content to release variant

--- app/src/release/java/com/zevrant/service/zevrantandroidapp/NukeSSLCerts.java
+++ app/src/release/java/com/zevrant/service/zevrantandroidapp/NukeSSLCerts.java
@@ -1,17 +1,15 @@
 package com.zevrant.services.zevrantandroidapp;
 
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLSocketFactory;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 public class NukeSSLCerts {
     protected static final String TAG = "NukeSSLCerts";
+
+    public static SSLSocketFactory getSslSocketFactory() {
+        return null;
+    }
+
 
     public static void nuke() {
         //do nothing only needed for local builds
